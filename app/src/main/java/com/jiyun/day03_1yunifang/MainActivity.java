51f0b555d9571876a6c3e524febc5998ce8b36a3
zhangkaiya <1319477202@qq.com>
2017-07-12 09:16:26 +0800

    御泥坊

---
+++ app/src/main/java/com/jiyun/day03_1yunifang/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jiyun.day03_1yunifang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
